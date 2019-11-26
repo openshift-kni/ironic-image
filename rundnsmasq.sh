0c5198c1d88bbb9bd1a9ecd8072fd98e5995ece3
Stephen Benjamin <stephen@redhat.com>
2019-11-26 09:49:06 -0500

    Provide a way to disable DHCP in dnsmasq
    If $DHCP_RANGE is empty, then we don't include the configuration items
    for DHCP in dnsmasq, only TFTP.
    
    Due to the increased complexity of the dnsmasq.conf configuration, this
    also changes the way we template the file to using Jinja2.

--- rundnsmasq.sh
+++ rundnsmasq.sh
@@ -2,8 +2,9 @@
 
 . /bin/ironic-common.sh
 
-HTTP_PORT=${HTTP_PORT:-"80"}
-DNSMASQ_EXCEPT_INTERFACE=${DNSMASQ_EXCEPT_INTERFACE:-"lo"}
+
+export HTTP_PORT=${HTTP_PORT:-"80"}
+export DNSMASQ_EXCEPT_INTERFACE=${DNSMASQ_EXCEPT_INTERFACE:-"lo"}
 
 wait_for_interface_or_ip
 
@@ -13,10 +14,11 @@ mkdir -p /shared/html/pxelinux.cfg
 mkdir -p /shared/log/dnsmasq
 
 # Copy files to shared mount
-cp /tftpboot/undionly.kpxe /tftpboot/ipxe.efi /tftpboot/snponly.efi /shared/tftpboot
+# TODO(stbenjam): Add snponly.efi to this list when it's available from EL8 packages.
+cp /tftpboot/undionly.kpxe /tftpboot/ipxe.efi /shared/tftpboot
 
 # Template and write dnsmasq.conf
-python -c 'import os; import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' </etc/dnsmasq.conf.j2 >/etc/dnsmasq.conf
+python3 -c 'import os; import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' </etc/dnsmasq.conf.j2 >/etc/dnsmasq.conf
 
 for iface in $( echo "$DNSMASQ_EXCEPT_INTERFACE" | tr ',' ' '); do
     sed -i -e "/^interface=.*/ a\except-interface=${iface}" /etc/dnsmasq.conf
